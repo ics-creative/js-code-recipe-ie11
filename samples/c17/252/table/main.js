023f9b98676e62211e7dae817860581f7bcaa26e
ics-kano <takeshi.kano@ics-web.jp>
2019-01-24 22:03:07 +0900

    サンプルフォルダーを最新版にする

--- samples/c17/252/table/main.js
+++ samples/c17/252/table/main.js
@@ -1,8 +1,3 @@
-// IE11対応用の追加：console.tableがないときは警告を出す
-if (typeof window.console.table == "undefined") {
-  alert("console.table()に未対応のブラウザーです");
-} // IE11対応用に追加終わり
-
 // console.table のサンプル
 const myArray = [
   { id: 100, name: '鈴木', age: 25 },

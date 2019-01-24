f9a1d49996d2151716f870d11783cf2ded2606bc
ics-kano <takeshi.kano@ics-web.jp>
2019-01-25 05:40:44 +0900

    サンプルの更新

--- samples/c07/123/main.js
+++ samples/c07/123/main.js
@@ -1,4 +1,13 @@
+// scrollX、scrollYがサポートされているかどうか
+// IE11では未対応なので、window.pageXOffset、window.pageYOffsetを使う
+// 参考：https://developer.mozilla.org/ja/docs/Web/API/Window/scrollX
+
+const supportScrollX = window.scrollX != null;
+
 // ウインドウ上でスクロールする毎に「スクロール」と座標が出力される
 window.addEventListener('scroll', () => {
-  console.log('スクロール', window.scrollX, window.scrollY);
+  const scrollXOffset = supportScrollX ? window.scrollX : window.pageXOffset;
+  const scrollYOffset = supportScrollX ? window.scrollY : window.pageYOffset;
+  console.log('スクロール', scrollXOffset, scrollYOffset);
 });
+
--- transpiled_samples/c07/123/main.js
+++ transpiled_samples/c07/123/main.js
@@ -1,4 +1,10 @@
-// ウインドウ上でスクロールする毎に「スクロール」と座標が出力される
+// scrollX、scrollYがサポートされているかどうか
+// IE11では未対応なので、window.pageXOffset、window.pageYOffsetを使う
+// 参考：https://developer.mozilla.org/ja/docs/Web/API/Window/scrollX
+var supportScrollX = window.scrollX != null; // ウインドウ上でスクロールする毎に「スクロール」と座標が出力される
+
 window.addEventListener('scroll', function () {
-  console.log('スクロール', window.scrollX, window.scrollY);
+  var scrollXOffset = supportScrollX ? window.scrollX : window.pageXOffset;
+  var scrollYOffset = supportScrollX ? window.scrollY : window.pageYOffset;
+  console.log('スクロール', scrollXOffset, scrollYOffset);
 });
